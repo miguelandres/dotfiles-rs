a1b662bec968e8eb8d5c40a14a316a2fa58f7433
Miguel Barreto <miguelandres@users.noreply.github.com>
2021-02-05 23:35:30 -0500

    Set up pre-commit and run it on all files.

--- rustybot_core/src/directive.rs
+++ rustybot_core/src/directive.rs
@@ -1,3 +1,24 @@
+// Copyright (c) 2021-2021 Miguel Barreto and others
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 extern crate yaml_rust;
 
 use std::collections;
@@ -7,89 +28,89 @@ pub type Settings = collections::HashMap<String, Setting>;
 
 #[derive(Clone, Debug)]
 pub enum Setting {
-  Boolean(bool),
-  String(String),
+    Boolean(bool),
+    String(String),
 }
 
 pub struct DirectiveData {
-  name: &'static str,
-  defaults: Settings,
+    name: &'static str,
+    defaults: Settings,
 }
 impl DirectiveData {
-  pub fn new(name: &'static str, defaults: Settings) -> DirectiveData {
-    DirectiveData { name, defaults }
-  }
-  pub fn name(&self) -> &str {
-    self.name
-  }
-  pub fn defaults(&self) -> &Settings {
-    &self.defaults
-  }
+    pub fn new(name: &'static str, defaults: Settings) -> DirectiveData {
+        DirectiveData { name, defaults }
+    }
+    pub fn name(&self) -> &str {
+        self.name
+    }
+    pub fn defaults(&self) -> &Settings {
+        &self.defaults
+    }
 }
 
 /// A parser for action steps, each directive represents a type of Action.
 pub trait Directive<'a, A: Action<'a>> {
-  fn name(&self) -> &str;
-  fn defaults(&self) -> &Settings;
-  fn get_action(
-    &'a self,
-    settings: &Settings,
-    yaml: &Yaml,
-  ) -> Result<Box<dyn Action<'a> + 'a>, String>;
+    fn name(&self) -> &str;
+    fn defaults(&self) -> &Settings;
+    fn get_action(
+        &'a self,
+        settings: &Settings,
+        yaml: &Yaml,
+    ) -> Result<Box<dyn Action<'a> + 'a>, String>;
 }
 
 pub trait Action<'a> {
-  fn execute(&self) -> Result<(), String>;
+    fn execute(&self) -> Result<(), String>;
 }
 
 pub fn get_boolean_setting(
-  name: &str,
-  context_settings: &Settings,
-  directive_defaults: &Settings,
+    name: &str,
+    context_settings: &Settings,
+    directive_defaults: &Settings,
 ) -> Result<bool, String> {
-  if let Setting::Boolean(b) = get_setting(name, context_settings, directive_defaults)? {
-    Ok(b)
-  } else {
-    Err(format!("Setting {} was found but is not boolean", name))
-  }
+    if let Setting::Boolean(b) = get_setting(name, context_settings, directive_defaults)? {
+        Ok(b)
+    } else {
+        Err(format!("Setting {} was found but is not boolean", name))
+    }
 }
 
 pub fn get_setting(
-  name: &str,
-  context_settings: &Settings,
-  directive_defaults: &Settings,
+    name: &str,
+    context_settings: &Settings,
+    directive_defaults: &Settings,
 ) -> Result<Setting, String> {
-  if let Some(setting) = context_settings.get(name) {
-    Ok(setting.clone())
-  } else if let Some(setting) = directive_defaults.get(name) {
-    Ok(setting.clone())
-  } else {
-    Err(format!(
-      "Setting {} couldn't be found in context or defaults",
-      name
-    ))
-  }
+    if let Some(setting) = context_settings.get(name) {
+        Ok(setting.clone())
+    } else if let Some(setting) = directive_defaults.get(name) {
+        Ok(setting.clone())
+    } else {
+        Err(format!(
+            "Setting {} couldn't be found in context or defaults",
+            name
+        ))
+    }
 }
 
 pub fn get_string_content_or_keyed_value(yaml: &Yaml, key: Option<&str>) -> Result<String, String> {
-  match (yaml, key) {
-    (Yaml::String(s), _) => Ok(s.clone()),
-    (Yaml::Hash(hash), Some(key)) => {
-      match hash.get(&Yaml::String(String::from(key))).ok_or(format!(
-        "Yaml hash does not contain key {}: {:?}",
-        key, hash
-      ))? {
-        Yaml::String(s) => Ok(s.clone()),
-        yaml => Err(format!(
-          "Yaml Hash contains key {} but its value is not a string: {:?}",
-          key, yaml
+    match (yaml, key) {
+        (Yaml::String(s), _) => Ok(s.clone()),
+        (Yaml::Hash(hash), Some(key)) => {
+            match hash.get(&Yaml::String(String::from(key))).ok_or(format!(
+                "Yaml hash does not contain key {}: {:?}",
+                key, hash
+            ))? {
+                Yaml::String(s) => Ok(s.clone()),
+                yaml => Err(format!(
+                    "Yaml Hash contains key {} but its value is not a string: {:?}",
+                    key, yaml
+                )),
+            }
+        }
+        (yaml, Some(key)) => Err(format!(
+            "Yaml value is not a string or does not contain key {}: {:?}",
+            key, yaml
         )),
-      }
+        (yaml, None) => Err(format!("Yaml value is not a string: {:?}", yaml)),
     }
-    (yaml, Some(key)) => Err(format!(
-      "Yaml value is not a string or does not contain key {}: {:?}",
-      key, yaml
-    )),
-    (yaml, None) => Err(format!("Yaml value is not a string: {:?}", yaml)),
-  }
 }
--- rustybot_core/tests/directive.rs
+++ rustybot_core/tests/directive.rs
@@ -1,64 +1,85 @@
 #![cfg(test)]
 
+// Copyright (c) 2021-2021 Miguel Barreto and others
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 extern crate yaml_rust;
 
 use rustybot_core::directive::*;
 use yaml_rust::YamlLoader;
 
 mod test_get_string_content_or_keyed_value {
-  use super::*;
-  const YAML_STRING: &str = "
+    use super::*;
+    const YAML_STRING: &str = "
 - create:
     - /home/user/somedir";
 
-  const YAML_HASH: &str = "
+    const YAML_HASH: &str = "
 - create:
     - dir: /home/user/somedir";
-  const YAML_HASH_WITHOUT_EXPECTED_KEY: &str = "
+    const YAML_HASH_WITHOUT_EXPECTED_KEY: &str = "
 - create:
     - what: /home/user/somedir";
 
-  #[test]
-  fn gets_bare_string_successfully() -> Result<(), String> {
-    let docs = YamlLoader::load_from_str(YAML_STRING).unwrap();
-    let doc = &docs[0];
-    let list = &doc[0]["create"][0];
-    assert_eq!(
-      "/home/user/somedir",
-      get_string_content_or_keyed_value(list, None)?
-    );
-    assert_eq!(
-      "/home/user/somedir",
-      get_string_content_or_keyed_value(list, Some("dir"))?
-    );
-    Ok(())
-  }
-  #[test]
-  fn gets_string_in_hash_successfully() -> Result<(), String> {
-    let docs = YamlLoader::load_from_str(YAML_HASH).unwrap();
-    let doc = &docs[0];
-    let list = &doc[0]["create"][0];
-    assert_eq!(
-      "/home/user/somedir",
-      get_string_content_or_keyed_value(list, Some("dir"))?
-    );
-    Ok(())
-  }
-  #[test]
-  #[should_panic]
-  fn fails_to_get_string_in_hash_when_no_key_was_provided() {
-    let docs = YamlLoader::load_from_str(YAML_HASH).unwrap();
-    let doc = &docs[0];
-    let list = &doc[0]["create"][0];
-    get_string_content_or_keyed_value(list, None).unwrap();
-  }
+    #[test]
+    fn gets_bare_string_successfully() -> Result<(), String> {
+        let docs = YamlLoader::load_from_str(YAML_STRING).unwrap();
+        let doc = &docs[0];
+        let list = &doc[0]["create"][0];
+        assert_eq!(
+            "/home/user/somedir",
+            get_string_content_or_keyed_value(list, None)?
+        );
+        assert_eq!(
+            "/home/user/somedir",
+            get_string_content_or_keyed_value(list, Some("dir"))?
+        );
+        Ok(())
+    }
+    #[test]
+    fn gets_string_in_hash_successfully() -> Result<(), String> {
+        let docs = YamlLoader::load_from_str(YAML_HASH).unwrap();
+        let doc = &docs[0];
+        let list = &doc[0]["create"][0];
+        assert_eq!(
+            "/home/user/somedir",
+            get_string_content_or_keyed_value(list, Some("dir"))?
+        );
+        Ok(())
+    }
+    #[test]
+    #[should_panic]
+    fn fails_to_get_string_in_hash_when_no_key_was_provided() {
+        let docs = YamlLoader::load_from_str(YAML_HASH).unwrap();
+        let doc = &docs[0];
+        let list = &doc[0]["create"][0];
+        get_string_content_or_keyed_value(list, None).unwrap();
+    }
 
-  #[test]
-  #[should_panic]
-  fn fails_to_get_string_in_hash_when_without_correct_key() {
-    let docs = YamlLoader::load_from_str(YAML_HASH_WITHOUT_EXPECTED_KEY).unwrap();
-    let doc = &docs[0];
-    let list = &doc[0]["create"][0];
-    get_string_content_or_keyed_value(list, None).unwrap();
-  }
+    #[test]
+    #[should_panic]
+    fn fails_to_get_string_in_hash_when_without_correct_key() {
+        let docs = YamlLoader::load_from_str(YAML_HASH_WITHOUT_EXPECTED_KEY).unwrap();
+        let doc = &docs[0];
+        let list = &doc[0]["create"][0];
+        get_string_content_or_keyed_value(list, None).unwrap();
+    }
 }
