abdb3264e14cea10cdf1de3f860ce45dc68e2d59
miguelandres <miguelandres@users.noreply.github.com>
2021-11-01 13:56:33 +0000

    deploy: 9ad5d6dbb1ffe27ddbf58d1814741defc3c479f8

---
+++ rustybot_core/link/action/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["LinkAction","[LinkAction] creates a new symlink `path` that points to `target`."]]});
---
+++ rustybot_core/link/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"mod":[["action","This module contains the [LinkAction] that creates a new symlink when executed"],["directive","This module defines [LinkDirective]."]]});
