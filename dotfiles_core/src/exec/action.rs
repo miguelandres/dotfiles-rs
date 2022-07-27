e82fdad38b23f4f2dce7b8782f2a4252dad3ca01
Miguel Barreto <miguelandres@users.noreply.github.com>
2022-07-27 00:47:55 -0400

    Add missing module comments

--- dotfiles_core/src/exec/action.rs
+++ dotfiles_core/src/exec/action.rs
@@ -19,6 +19,8 @@
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+//! This module contains the [ExecAction] that executes a command in the shell
+
 use std::marker::PhantomData;
 
 use subprocess::Exec;
--- dotfiles_core/src/exec/directive.rs
+++ dotfiles_core/src/exec/directive.rs
@@ -19,6 +19,8 @@
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+//! This module defines [ExecDirective] which represents commands to execute in a shell.
+
 use std::{collections, marker::PhantomData};
 
 use yaml_rust::Yaml;
