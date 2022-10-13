97395609dbbdbb7077ffd63b19c6f32e5f19be3a
miguelandres <miguelandres@users.noreply.github.com>
2022-10-13 04:33:56 +0000

    deploy: ab60b4258923e121608537253dbe829919c0559a

--- source-files.js
+++ source-files.js
@@ -3,6 +3,6 @@ var sourcesIndex = JSON.parse('{\
 "dotfiles_actions":["",[["brew",[],["action.rs","directive.rs","mod.rs"]],["create",[],["action.rs","directive.rs","mod.rs"]],["exec",[],["action.rs","directive.rs","mod.rs"]],["homebrew_install",[],["action.rs","mod.rs"]],["link",[],["action.rs","directive.rs","mod.rs"]],["ohmyzsh_install",[],["action.rs","mod.rs"]]],["lib.rs"]],\
 "dotfiles_core":["",[],["action.rs","directive.rs","error.rs","exec_wrapper.rs","lib.rs","settings.rs","yaml_util.rs"]],\
 "dotfiles_core_macros":["",[],["action_list.rs","lib.rs"]],\
-"dotfiles_processor":["",[],["flags.rs","lib.rs","process.rs"]]\
+"dotfiles_processor":["",[],["context.rs","flags.rs","lib.rs","process.rs"]]\
 }');
 createSourceSidebar();
