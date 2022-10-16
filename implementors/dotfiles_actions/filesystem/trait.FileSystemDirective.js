67593537e2f650dcfe08ae7e8aca660cb5acc47e
miguelandres <miguelandres@users.noreply.github.com>
2022-10-16 02:22:59 +0000

    deploy: b45e0a6fcc20f11aa78f397a9043b3718a278554

---
+++ implementors/dotfiles_actions/filesystem/trait.FileSystemDirective.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"dotfiles_actions":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
