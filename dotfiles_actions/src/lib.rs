b63b407e9b7295eff85efecfe235cd90801e9370
Miguel Barreto <miguelandres@users.noreply.github.com>
2023-06-17 22:04:47 -0400

    remove feature drain filter which made it to stable

--- dotfiles_actions/src/lib.rs
+++ dotfiles_actions/src/lib.rs
@@ -22,7 +22,6 @@
 #![warn(missing_docs)]
 #![deny(rustdoc::broken_intra_doc_links)]
 #![feature(io_error_more)]
-#![feature(drain_filter)]
 #![feature(slice_concat_trait)]
 
 //! This crate contains all concrete [actions](dotfiles_core::action::Action)
