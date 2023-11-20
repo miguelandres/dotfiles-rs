562e31c1f0eecdd1135217c54ff821b74e04fc10
miguelandres <miguelandres@users.noreply.github.com>
2023-11-20 13:49:53 +0000

    deploy: 7766982ff754353411cbf77b96c2a0229a0abfe0

--- implementors/core/cmp/trait.PartialOrd.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"dotfiles_processor":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a>&lt;<a class=\"enum\" href=\"dotfiles_processor/flags/enum.LogLevelFilter.html\" title=\"enum dotfiles_processor::flags::LogLevelFilter\">LogLevelFilter</a>&gt; for <a class=\"enum\" href=\"dotfiles_processor/flags/enum.LogLevelFilter.html\" title=\"enum dotfiles_processor::flags::LogLevelFilter\">LogLevelFilter</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
