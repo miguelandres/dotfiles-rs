7ed3c1b59a3b9165be6911efd075b1f559145ae5
miguelandres <miguelandres@users.noreply.github.com>
2024-02-08 01:55:25 +0000

    deploy: 88119f23f93b0377f3e568d622bd255dff9663d2

--- dotfiles_actions/brew/directive/sidebar-items.js
+++ dotfiles_actions/brew/directive/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["CASK_SETTING","DIRECTIVE_NAME","FORCE_CASKS_SETTING","FORMULA_SETTING","TAP_SETTING"],"fn":["init_directive_data"],"struct":["BrewDirective"]};
+window.SIDEBAR_ITEMS = {"constant":["ADOPT_CASKS_SETTING","CASK_SETTING","DIRECTIVE_NAME","FORCE_CASKS_SETTING","FORMULA_SETTING","TAP_SETTING"],"fn":["init_directive_data"],"struct":["BrewDirective"]};
