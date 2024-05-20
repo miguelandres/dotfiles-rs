6125cb4606047c785d886681e95e0e2fcbbcf120
miguelandres <miguelandres@users.noreply.github.com>
2024-05-20 02:32:50 +0000

    deploy: 2aa2577314fbee1c065c53b7f114aecee0fbacc5

---
+++ search.desc/dotfiles/dotfiles-desc-0-.js
@@ -0,0 +1,1 @@
+searchState.loadedDescShard("dotfiles", 0, "")
---
+++ search.desc/dotfiles_core_macros/dotfiles_core_macros-desc-0-.js
@@ -0,0 +1,1 @@
+searchState.loadedDescShard("dotfiles_core_macros", 0, "This crate provides procedural macros to generate …\nGenerates an implementation of ConditionalAction&lt;’a&gt; …\nGenerates a Directive&lt;’a&gt; and a HasDirectiveData&lt;’a&gt; …")
